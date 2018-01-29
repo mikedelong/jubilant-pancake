637398582e2721cdbf4ea1f106cb99a1029f9ae6
mikedelong <mikedelong@outlook.com>
2018-01-29 07:37:56 -0500

    feature: logging setup, settings read. This is all boilerplate code copied from the script we're using as a baseline.

---
+++ filter_for_active.py
@@ -0,0 +1,27 @@
+import collections
+import csv
+import datetime
+import json
+import logging
+
+# set up logging
+formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+console_handler.setLevel(logging.DEBUG)
+logger.debug('started')
+
+
+# read the input filename from a JSON file
+settings_file = './settings.json'
+logger.debug('settings file : %s' % settings_file)
+with open(settings_file, 'r') as settings_fp:
+    settings = json.load(settings_fp)
+
+logger.debug('settings: %s' % settings)
+input_file = settings['input_file']
+logger.debug('input file: %s' % input_file)
+
