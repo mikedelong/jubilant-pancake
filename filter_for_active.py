050b0bd285944fdc7e272e1e3a2c61fea55a9cdb
mikedelong <mikedelong@outlook.com>
2018-01-29 07:48:29 -0500

    feature: load the filter data from the appropriate sheet in the filter data Excel file and log the head of the resulting data frame.

--- filter_for_active.py
+++ filter_for_active.py
@@ -1,8 +1,7 @@
-import collections
-import csv
-import datetime
 import json
 import logging
+
+import pandas as pd
 
 # set up logging
 formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
@@ -22,6 +21,16 @@ with open(settings_file, 'r') as settings_fp:
     settings = json.load(settings_fp)
 
 logger.debug('settings: %s' % settings)
+
+# let's do everything to get our filter data before we load anything else
+filter_file = settings['filter_file']
+logger.debug('filter file: %s' % filter_file)
+filter_sheet_name = settings['filter_sheet_name']
+logger.debug('filter sheet name: %s' % filter_sheet_name)
+filter_data = pd.read_excel(filter_file, sheet_name=filter_sheet_name)
+
+logger.debug(filter_data.head())
+
+# now let's load the big input file
 input_file = settings['input_file']
 logger.debug('input file: %s' % input_file)
-
