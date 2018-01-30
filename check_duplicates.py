9d3656da710f12f451c84d9db5d629b54acdf392
mikedelong <mikedelong@outlook.com>
2018-01-30 10:42:33 -0500

    feature: add a day-of-year column to our dataframe

--- check_duplicates.py
+++ check_duplicates.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import logging
 import time
@@ -5,6 +6,22 @@ import time
 import pandas as pd
 
 start_time = time.time()
+
+
+def make_date(arg_date):
+    day = int(arg_date)
+    if day > 366:
+        date_day = int(arg_date[-3:])
+        date_year = int(arg_date[0:-3])
+        year = 1900 + date_year if date_year > 50 else 2000 + date_year
+        date = datetime.date(year, 1, 1) + datetime.timedelta(days=date_day)
+    else:
+        date_day = int(arg_date)
+        date = datetime.date(2000, 1, 1) + datetime.timedelta(days=date_day)
+
+    return date
+
+
 # set up logging
 formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
 logger = logging.getLogger('main')
@@ -28,12 +45,17 @@ logger.debug('settings: %s' % settings)
 input_file = settings['input_file']
 logger.debug('input file: %s' % input_file)
 
-data = pd.read_csv(input_file)
+converters = {'serial_number': str, 'SORTIE_DATE': str, 'HOURS': float}
+data = pd.read_csv(input_file, converters=converters)
 logger.debug('input file read complete.')
 
 # we want to remove the second column because it doesn't tell us anything
+data_columns = data.columns
+logger.debug('our big data frame has column headers %s' % data.columns)
+
 column_to_drop = data.columns[1]
-logger.debug('before looing for duplicates we are going to drop column %s' % column_to_drop)
+date_column = data.columns[2]  # note that this is before we drop the column above
+logger.debug('before looking for duplicates we are going to drop column %s' % column_to_drop)
 data.drop(column_to_drop, axis=1, inplace=True)
 
 # how many columns do we have before dropping?
@@ -45,6 +67,12 @@ logger.debug('after dropping duplicates we have shape %s ' % str(data.shape))
 count_after = data.shape[0]
 
 logger.debug('this means we have %d duplicate rows' % (count_before - count_after))
+
+# add the day-of-year column
+data['dayOfYear'] = data[date_column].astype(int)
+logger.debug('after adding the day of the year we have shape %s' % str(data.shape))
+logger.debug('after adding the day of the year our columns are %s' % data.columns)
+# now add the date column
 
 logger.debug('done')
 finish_time = time.time()
