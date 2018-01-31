f4dfe73394691fca317a5440f9af16ed33b348ea
mikedelong <mikedelong@outlook.com>
2018-01-31 14:30:57 -0500

    feature: initial check-in of code to read our clean data

---
+++ sum_by_year.py
@@ -0,0 +1,27 @@
+import logging
+import time
+
+import pandas as pd
+
+start_time = time.time()
+
+# set up logging
+formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+console_handler.setLevel(logging.DEBUG)
+logger.debug('started')
+
+# todo fix these
+input_folder = './output/'
+input_file = 'nozeros.csv'
+
+full_input_file = input_folder + input_file
+
+logger.debug('reading input data from %s' % full_input_file)
+data = pd.read_csv(full_input_file)
+logger.debug('data read complete.')
+logger.debug(data.head(20))
