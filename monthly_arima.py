b55fed67a780c976649b30f13cc76aad80edc5cf
mikedelong <mikedelong@outlook.com>
2018-02-05 11:17:58 -0500

    feature: shell with logging and total time reporting.

--- monthly_arima.py
+++ monthly_arima.py
@@ -1,1 +1,34 @@
 # https://machinelearningmastery.com/arima-for-time-series-forecasting-with-python/
+
+import json
+import logging
+import time
+
+start_time = time.time()
+
+# set up logging
+formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+console_handler.setLevel(logging.DEBUG)
+logger.debug('started')
+
+# read the input filename from a JSON file
+settings_file = './settings.json'
+logger.debug('settings file : %s' % settings_file)
+with open(settings_file, 'r') as settings_fp:
+    settings = json.load(settings_fp)
+
+logger.debug('settings: %s' % settings)
+
+# todo work goes here
+
+
+logger.debug('done')
+finish_time = time.time()
+elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
+elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
+logger.info("Time: {:0>2}:{:0>2}:{:05.2f}".format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
