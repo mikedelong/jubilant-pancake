cc7675a180c09319b9095a26522b33f4d4013f8f
mikedelong <mikedelong@outlook.com>
2018-02-05 12:32:30 -0500

    feature: group and sum to get monthly totals. There is probably a better way to do this, but we are starting with data at the tail level and ending with data at the month level.

--- monthly_arima.py
+++ monthly_arima.py
@@ -37,7 +37,7 @@ logger.debug('settings: %s' % settings)
 input_folder = settings['processed_folder']
 full_input_file = input_folder + 'monthly.csv'
 logger.debug('reading input data from %s' % full_input_file)
-data = pd.read_csv(full_input_file, index_col=['tail'])
+data = pd.read_csv(full_input_file)
 
 # now we want to discard anything before 2010 and after 2016
 data = data[data['year'].astype(int) >= 2010]
@@ -45,7 +45,15 @@ data = data[data['year'].astype(int) <= 2016]
 
 # todo think about pushing this back into the upstream code
 data['date'] = np.vectorize(make_date)(data['year'].astype(int), data['month'].astype(int))
+logger.debug('after trimming to the date range of interest we have shape %s ' % str(data.shape))
+
+# to get just the data we want lets throw out the tail, month, and date
+data.drop(['tail', 'year', 'month'], axis=1, inplace=True)
+logger.debug('after dropping columns we have shape %s ' % str(data.shape))
 logger.debug(data.head(5))
+
+t0 = data.groupby(['date'], axis=0).sum()
+logger.debug(t0.head(5))
 
 logger.debug('done')
 finish_time = time.time()
