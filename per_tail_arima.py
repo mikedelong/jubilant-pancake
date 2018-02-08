a0179956247f89f6dbd19bd25ac8b874f3015137
mikedelong <mikedelong@outlook.com>
2018-02-08 14:57:39 -0500

    feature: shell, no actual working analytics code.

--- per_tail_arima.py
+++ per_tail_arima.py
@@ -1,1 +1,49 @@
 # https://machinelearningmastery.com/arima-for-time-series-forecasting-with-python/
+
+import datetime
+import json
+import logging
+import time
+
+import pandas as pd
+import pandas.tseries.offsets as offsets
+
+start_time = time.time()
+
+
+def make_date(arg_year, arg_month):
+    result = pd.to_datetime(datetime.date(arg_year, arg_month, 1) + offsets.MonthEnd(0))
+    return result
+
+
+def project_dates(arg_base, arg_count):
+    result = [pd.to_datetime(datetime.date(arg_base.year, arg_base.month, 1) + offsets.MonthEnd(month)) for month in
+              range(1, arg_count + 1)]
+    return result
+
+
+# set up logging
+formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+console_handler.setLevel(logging.DEBUG)
+logger.debug('started')
+
+# read the input filename from a JSON file
+settings_file = './settings.json'
+logger.debug('settings file : %s' % settings_file)
+with open(settings_file, 'r') as settings_fp:
+    settings = json.load(settings_fp)
+
+logger.debug('settings: %s' % settings)
+
+# code goes here
+
+logger.debug('done')
+finish_time = time.time()
+elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
+elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
+logger.info("Time: {:0>2}:{:0>2}:{:05.2f}".format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
