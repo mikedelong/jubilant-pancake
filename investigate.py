e4501eadbac92401dcd5ddda79eaf8e0eed6151b
mikedelong <mikedelong@outlook.com>
2018-02-14 09:24:26 -0500

    new file: shell for investigating new data set

---
+++ investigate.py
@@ -0,0 +1,49 @@
+import json
+import logging
+import time
+
+import pandas as pd
+
+start_time = time.time()
+# set up logging
+formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+console_handler.setLevel(logging.DEBUG)
+logger.debug('started')
+
+# read the input filename from a JSON file
+settings_file = './investigate.json'
+logger.debug('settings file : %s' % settings_file)
+with open(settings_file, 'r') as settings_fp:
+    settings = json.load(settings_fp)
+
+logger.debug('settings: %s' % settings)
+
+# now let's load the big input file
+input_folder = settings['input_folder']
+logger.debug('input folder: %s' % input_folder)
+input_file = settings['input_file']
+logger.debug('input file: %s' % input_file)
+full_input_file = input_folder + input_file
+logger.debug('reading input data from %s' % full_input_file)
+data = pd.read_csv(full_input_file)
+logger.debug('read complete: columns are %s' % str(data.columns))
+for column in data.columns:
+    unique_value_count = data[column].nunique()
+    if unique_value_count > 10:
+        logger.debug('column %s has %d values and %d unique values.' %
+                     (column, len(data[column]), data[column].nunique()))
+    else:
+        unique_values = data[column].unique()
+        values = [item.strip() for item in unique_values]
+        logger.debug('column %s has the following unique values: %s' % (column, values))
+
+logger.debug('done')
+finish_time = time.time()
+elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
+elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
+logger.info("Time: {:0>2}:{:0>2}:{:05.2f}".format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
