7f5cc61db28d24a3fc7852c492478be75ccc5237
mikedelong <mikedelong@outlook.com>
2018-02-15 13:21:51 -0500

    feature: columns to use; we use this to whitelist columns we want to use and ignore the rest. Moved the actual list of column names to our JSON settings file.

--- investigate.py
+++ investigate.py
@@ -30,13 +30,16 @@ input_file = settings['input_file']
 logger.debug('input file: %s' % input_file)
 full_input_file = input_folder + input_file
 logger.debug('reading input data from %s' % full_input_file)
-data = pd.read_csv(full_input_file)
+
+columns_to_use = settings['columns_to_use']
+
+data = pd.read_csv(full_input_file, skipinitialspace=True, skip_blank_lines=True, usecols=columns_to_use)
 logger.debug('read complete: columns are %s' % str(data.columns))
 logger.debug('data shape is %d x %d' % data.shape)
 for column in data.columns:
     unique_value_count = data[column].nunique()
     logger.debug('column %s has %d values and %d unique values.' % (column, len(data[column]), data[column].nunique()))
-    if unique_value_count < 21:
+    if unique_value_count < 101:
         logger.debug('and here they are: %s' % data[column].unique())
 logger.debug('done')
 finish_time = time.time()
