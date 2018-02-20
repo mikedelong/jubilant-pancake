11ebc3f5feba422cf3b11faa77ee4f0885fe8609
mikedelong <mikedelong@outlook.com>
2018-02-20 10:51:56 -0500

    feature: added short summary of where we are with bad dates.

--- investigate.py
+++ investigate.py
@@ -63,21 +63,28 @@ data['tail'] = np.vectorize(make_tail)(data[serial])
 
 # now let's look for cases where we can fill in the year
 bad_date_records = data[data[reference_date].astype(int) < 366]
-logger.debug('rows with bad date have shape: %d x %d' % data[data[reference_date].astype(int) < 366].shape)
+logger.debug('rows with bad date have shape: %d x %d' % bad_date_records.shape)
 year_field = settings['year_field']
 logger.debug('Rows with bad dates have %s unique year-field values' % bad_date_records[year_field].unique())
-logger.debug(
-    'Rows with bad dates have the following year-field value counts: %s' % bad_date_records[year_field].value_counts())
+logger.debug('Rows with bad dates have the following year-field value counts: %s' %
+             bad_date_records[year_field].value_counts())
 bad_date_year_valid = bad_date_records[bad_date_records[year_field].astype(int) > 1947]
-logger.debug('rows with bad date and not-null year have shape: %d x %d' % bad_date_year_valid.shape)
-month_field = settings['month_field']
-logger.debug('rows with bad date and not-null month have shape: %d x %d' % data[
-    data[reference_date].astype(int) < 366 & ~data[month_field].empty].shape)
-logger.debug('rows with bad date and not-null start date shape: %d x %d' % data[
-    data[reference_date].astype(int) < 366 & ~data['start_date_and_time'].empty].shape)
-logger.debug('rows with bad date and not-null process timestamp shape: %d x %d' % data[
-    data[reference_date].astype(int) < 366 & ~data['PROCESS_TIMESTAMP'].empty].shape)
 
+missing_date_records = data[data[reference_date].astype(int) == 0]
+logger.debug('rows with zero date have shape %d x %d' % missing_date_records.shape)
+
+if False:
+    logger.debug('rows with bad date and not-null year have shape: %d x %d' % bad_date_year_valid.shape)
+    month_field = settings['month_field']
+    logger.debug('rows with bad date and not-null month have shape: %d x %d' % data[
+        data[reference_date].astype(int) < 366 & ~data[month_field].empty].shape)
+    logger.debug('rows with bad date and not-null start date shape: %d x %d' % data[
+        data[reference_date].astype(int) < 366 & ~data['start_date_and_time'].empty].shape)
+    logger.debug('rows with bad date and not-null process timestamp shape: %d x %d' % data[
+        data[reference_date].astype(int) < 366 & ~data['PROCESS_TIMESTAMP'].empty].shape)
+
+logger.debug('so we have %d rows with missing year and %d with no date' %
+             (bad_date_records.shape[0], missing_date_records.shape[0]))
 logger.debug('done')
 finish_time = time.time()
 elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
