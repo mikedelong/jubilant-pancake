7d3adbfdc6a4ac86d3cda2c631103b8c9f033224
mikedelong <mikedelong@outlook.com>
2018-02-22 12:49:32 -0500

    feature: a first cut at projecting 2018 using ARIMA

--- per_tail_arima.py
+++ per_tail_arima.py
@@ -88,7 +88,7 @@ forecast_count = 0
 tails = max_dates[max_dates[months_count] > 0].index.values.tolist()
 ey2016 = list()
 ey2017 = list()
-
+ey2018 = list()
 for tail in tails:
     # todo review this
     tail_data = data.loc[data['tail'] == tail]
@@ -101,27 +101,43 @@ for tail in tails:
         senior_count += 1
         logger.debug('count: %d tail %s has %.1f hours and will be excluded.' % (senior_count, tail, before_value))
         ey2017.extend([before_value])
+        ey2018.extend([before_value])
     else:
         forecast_count += 1
         # for the model to work properly we need the dates to be the index
-        model = ARIMA(tail_data, order=(order_d, 1, 0))
-        model_fit = model.fit(disp=0)
+        model_2017 = ARIMA(tail_data, order=(order_d, 1, 0))
+        model_2017_fit = model_2017.fit(disp=0)
         # now let's forecast for 2017
-        steps = max_dates.loc[tail, months_count] + 12
-        forecast = model_fit.forecast(steps=steps)
+        forecast_2017 = model_2017_fit.forecast(steps=max_dates.loc[tail, months_count] + 12)
         # this will give us the pre-2010 hours plus the ARIMA model's training data hours
-        after_value = before_value + forecast[0].sum()
-        if after_value > 8000:
+        after_value_2017 = before_value + forecast_2017[0].sum()
+        if after_value_2017 > 8000:
             forecast_senior_count += 1
-        logger.debug('tail %s EY2016: %.1f EY2017: %.1f' % (tail, before_value, after_value))
-        # todo see if we can calculate this outside the loop
-        ey2017.extend([after_value])
+        logger.debug('tail %s EY2016: %.1f EY2017: %.1f' % (tail, before_value, after_value_2017))
+        ey2017.extend([after_value_2017])
+
+        # for the model to work properly we need the dates to be the index
+        model_2018 = ARIMA(tail_data, order=(order_d, 1, 0))
+        model_2018_fit = model_2018.fit(disp=0)
+        # now let's forecast for 2018
+        forecast_2018 = model_2018_fit.forecast(steps=max_dates.loc[tail, months_count] + 24)
+        # this will give us the pre-2010 hours plus the ARIMA model's training data hours
+        after_value_2018 = before_value + forecast_2018[0].sum()
+        if after_value_2018 > 8000:
+            forecast_senior_count += 1
+        logger.debug('tail %s EY2016: %.1f EY2018: %.1f' % (tail, before_value, after_value_2018))
+        ey2018.extend([after_value_2018])
 
 ey2016_over_8000 = [item > 8000 for item in ey2016]
 ey2017_over_8000 = [item > 8000 for item in ey2017]
+ey2018_over_8000 = [item > 8000 for item in ey2018]
+
 output = pd.DataFrame.from_dict(
-    {'tail': tails, 'EY2016': ey2016, 'EY16over8k': ey2016_over_8000, 'EY2017': ey2017,
-                                 'EY17over8k': ey2017_over_8000})
+    {'tail': tails,
+     'EY2016': ey2016, 'EY16over8k': ey2016_over_8000,
+     'EY2017': ey2017, 'EY17over8k': ey2017_over_8000,
+     'EY2018': ey2018, 'EY18over8k': ey2018_over_8000,
+     })
 
 logger.debug('forecast: %d, over 8000 hours: %d, not flown recently: %d.' % (forecast_count, senior_count, stale_count))
 logger.debug('forecast %d will be over 8000 hours at end of year.' % (senior_count + forecast_senior_count))
